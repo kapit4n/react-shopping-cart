0ebfde8b6df5c6765696a08957d4be19566abd93
lartl <luis.arce22@gmail.com>
2018-08-15 20:38:18 -0400

    Creating login page

---
+++ src/components/LoginComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoginComponent = () => {
+  return <div>
+    <div className='form-group'>
+      <label for='login'>Login</label>
+      <input type='email' className='form-control' id='login' planceholder='Login' />
+    </div>
+    
+    <div className='form-group'>
+      <label for='password'>Password</label>
+      <input type='password' className='form-control' id='password' planceholder='Password' />
+    </div>
+    <button className='btn btn-primary'>Login</button>
+  </div>
+}
---
+++ src/containers/Login.js
@@ -0,0 +1,6 @@
+import react from 'react';
+import LoginComponent from '../components/LoginComponent'
+
+class ProductList extends react.Component {
+    render return <LoginComponent></LoginComponent>
+}
