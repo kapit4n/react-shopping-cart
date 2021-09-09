b86b5c787f40845d9dc0fa9f4e1e10c4377f6541
Luis Arce <luis.arce22@gmail.com>
2021-09-08 21:13:58 -0400

    tests: nav, cartInfo, home

---
+++ src/containers/cart/__tests__/Home-test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import TestCompoent from '../Home'
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Enzyme, { shallow, mount } from 'enzyme';
+// Import Adapter
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+//use adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+const store = createStore(r => r, {
+  products: {
+    todoApp: {
+      products: [],
+      cartInfo: {
+        total: 10
+      },
+      filters: {
+        display: true
+      }
+    }
+  }
+})
+
+describe('shallow', () => {
+  const wrapper = shallow(<TestCompoent store={store} />)
+  it('', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('mount', () => {
+  const wrapper = mount(
+    <Provider store={store}>
+      <Router>
+        <TestCompoent />
+      </Router>
+    </Provider>
+  )
+  it('', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
